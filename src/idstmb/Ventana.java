aa1e7fe836510c1c93c8a60887eb19500c0863c0
FgreenZ <fgreen_24@alu.uabcs.mx>
2026-02-21 19:18:11 -0700

    inicio de sesion

--- src/idstmb/Ventana.java
+++ src/idstmb/Ventana.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class Ventana extends JFrame {
 
@@ -18,7 +19,7 @@ public class Ventana extends JFrame {
 		this.setSize(600,600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		this.setBackground(Color.BLACK);
+		this.setBackground(Color.WHITE);
 		this.setMinimumSize(new Dimension(500,500));
 		this.setMaximumSize(new Dimension(600,600));
 		this.setTitle("Hola");
@@ -27,37 +28,101 @@ public class Ventana extends JFrame {
 		
 		JPanel contenedor = new JPanel();
 		contenedor.setOpaque(true);
-		contenedor.setBackground(Color.BLACK);
+		contenedor.setBackground(Color.DARK_GRAY);
 		contenedor.setSize(600,600);
 		contenedor.setLocation(0,0);
 		contenedor.setLayout(null);
 		this.add(contenedor);
 		
+		/*TITULO*/
 		JLabel title_login = new JLabel();
-		title_login.setText("hola bola");
-		title_login.setSize(100, 20);
+		title_login.setText("< ACCEDER >");
+		title_login.setSize(200, 25);
 		title_login.setOpaque(true);
-		title_login.setBackground(Color.cyan);
-		title_login.setLocation(250, 0);
-		title_login.setFont(new  Font("Arial",Font.BOLD,22));
+		title_login.setBackground(null);
+		title_login.setForeground(Color.WHITE);
+		title_login.setLocation(200, 60);
+		title_login.setFont(new  Font("Arial",Font.ROMAN_BASELINE,30));
 		contenedor.add(title_login);
 		
+		/*USERNAME*/
+		//Cuadro de texto
 		JTextField username=new JTextField();
-		username.setSize(300, 30);
-		username.setLocation(100, 250);
+		username.setSize(550, 30);
+		username.setLocation(20, 170);
 		username.setBackground(Color.WHITE);
 		username.setFont(new  Font("Arial",Font.BOLD,16));
 		contenedor.add(username);
+		//Texto
+		JLabel usernameT = new JLabel();
+		usernameT.setText("Nombre de usuario");
+		usernameT.setSize(350, 15);
+		usernameT.setOpaque(true);
+		usernameT.setBackground(null);
+		usernameT.setForeground(Color.WHITE);
+		usernameT.setLocation(20, 154);
+		usernameT.setFont(new  Font("Arial",Font.ITALIC,15));
+		contenedor.add(usernameT);
 		
-		JButton acceder= new JButton();
-		acceder.setText("boton");
-		acceder.setSize(100, 100);
-		acceder.setLocation(200,300);
-		acceder.setFont(new  Font("Arial",Font.BOLD,16));
-		contenedor.add(acceder);
+		/*PASSWORD*/
+		//Cuadro de texto
+		JTextField contraseña=new JTextField();
+		contraseña.setSize(550, 30);
+		contraseña.setLocation(20, 250);
+		contraseña.setBackground(Color.WHITE);
+		contraseña.setFont(new  Font("Arial",Font.BOLD,16));
+		contenedor.add(contraseña);
+		//Texto
+		JLabel contraseñaT = new JLabel();
+		contraseñaT.setText("Contraseña");
+		contraseñaT.setSize(350, 15);
+		contraseñaT.setOpaque(true);
+		contraseñaT.setBackground(null);
+		contraseñaT.setForeground(Color.WHITE);
+		contraseñaT.setLocation(20, 234);
+		contraseñaT.setFont(new  Font("Arial",Font.ITALIC,15));
+		contenedor.add(contraseñaT);
+																	
 
+		/*REMEMEBER ME*/
+		//Boton
+		JButton recuerdame= new JButton();
+		recuerdame.setSize(18, 18);
+		recuerdame.setLocation(20,300);
+		recuerdame.setFont(new  Font("Arial",Font.BOLD,16));
+		recuerdame.setBorder(new LineBorder(Color.BLACK, 2, false));
+		contenedor.add(recuerdame);
+		//Texto
+		JLabel recuerdameT = new JLabel();
+		recuerdameT.setText("Recordarme");
+		recuerdameT.setSize(100, 15);
+		recuerdameT.setOpaque(true);
+		recuerdameT.setBackground(null);
+		recuerdameT.setForeground(Color.WHITE);
+		recuerdameT.setLocation(44, 301);
+		recuerdameT.setFont(new  Font("Arial",Font.ITALIC,13));
+		contenedor.add(recuerdameT);
 		
+		/*Boton de acceder*/
+		JButton acceder= new JButton();
+		JTextField borde= new JTextField();
+		acceder.setText("Aceptar");
+		acceder.setSize(120, 90);
+		acceder.setLocation(250,370);
+		acceder.setFont(new  Font("Arial",Font.ROMAN_BASELINE,16));
+		acceder.setBorder(new LineBorder(Color.gray, 10, false));
+		contenedor.add(acceder);
 		
+		/*CONTRASEÑA OLVIDADA*/
+		JLabel contraseñaOlvidada = new JLabel();
+		contraseñaOlvidada.setText("<html><u>Olvide mi contraseña</u></html>");
+		contraseñaOlvidada.setSize(140, 50);
+		contraseñaOlvidada.setOpaque(true);
+		contraseñaOlvidada.setBackground(null);
+		contraseñaOlvidada.setForeground(Color.WHITE);
+		contraseñaOlvidada.setLocation(450, 283);
+		contraseñaOlvidada.setFont(new  Font("Arial",Font.ITALIC,13));
+		contenedor.add(contraseñaOlvidada);
 		
 		
 		contenedor.repaint();
@@ -66,6 +131,21 @@ public class Ventana extends JFrame {
 		
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
