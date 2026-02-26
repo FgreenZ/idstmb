f4c596a58536941a11664163378b477fc3896a84
FgreenZ <fgreen_24@alu.uabcs.mx>
2026-02-26 10:04:39 -0700

    Biografia terminada

--- src/idstmb/Ventana.java
+++ src/idstmb/Ventana.java
@@ -4,11 +4,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
@@ -17,12 +21,12 @@ public class Ventana extends JFrame {
 	public Ventana() {
 		
 		this.setVisible(true);
-		this.setSize(1200, 600);
+		this.setSize(1220, 640);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setBackground(Color.WHITE);
-		this.setMinimumSize(new Dimension(500,500));
-		this.setMaximumSize(new Dimension(600,600));
+		//this.setMinimumSize(new Dimension(1200,600));
+		//this.setMaximumSize(new Dimension(1200,600));
 		this.setTitle("Inicio de sesion");
 		this.setLocation(100,100);
 		this.setLayout(null);
@@ -127,33 +131,178 @@ public class Ventana extends JFrame {
 		
 		//contenedor de registro
 		JPanel register_conteneiner = new JPanel();
-		register_conteneiner.setOpaque(true);
-		register_conteneiner.setBackground(Color.BLUE);
 		register_conteneiner.setSize(600,600);
 		register_conteneiner.setLocation(600,0);
+		register_conteneiner.setOpaque(true);
+		register_conteneiner.setBackground(Color.lightGray);
 		register_conteneiner.setLayout(null);
 		this.add(register_conteneiner);
 		
-	
-		JLabel bio_tag=new JLabel("Biografia");
-		bio_tag.setBounds(100,100,250,40);
-		bio_tag.setBackground(Color.white);
-		bio_tag.setHorizontalAlignment(JLabel.CENTER);
-		bio_tag.setOpaque(true);
-		bio_tag.setFont(new  Font("Arial",Font.ITALIC,22));
-		register_conteneiner.add(bio_tag);
+		//biografia
+		JLabel titulo=new JLabel("Biografia");
+		titulo.setSize(250,80);
+		titulo.setBackground(Color.white);
+		titulo.setHorizontalAlignment(JLabel.CENTER);
+		titulo.setBackground(null);
+		titulo.setLocation(170,20);
+		titulo.setOpaque(true);
+		titulo.setFont(new  Font("Arial",Font.ITALIC,40));
+		register_conteneiner.add(titulo);
+		
+		//desplegable colonias
+		JComboBox colonias=new JComboBox();
+		colonias.setSize(550,20);
+		colonias.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] {"Camino Real", "Paraiso del Sol","El Pedregal", "Fidepaz", "Puerta Cortés", "Colinas del Sol"}));		
+		colonias.setOpaque(true);
+		colonias.setFont(new  Font("Arial",Font.PLAIN,16));
+		colonias.setLocation(20,455);
+		register_conteneiner.add(colonias);
+
+		//boton aceptar
+		JButton accederB= new JButton();
+		accederB.setText("Aceptar");
+		accederB.setSize(500, 90);
+		accederB.setBackground(Color.WHITE);
+		accederB.setFont(new  Font("Arial",Font.ROMAN_BASELINE,22));
+		accederB.setBorder(new LineBorder(Color.BLACK, 6, true));
+		accederB.setLocation(50,492);
+		register_conteneiner.add(accederB);
+		
+		//usuario
+		JTextField usuarioT=new JTextField();
+		usuarioT.setSize(550, 30);
+		usuarioT.setLocation(20, 155);
+		usuarioT.setBackground(Color.WHITE);
+		usuarioT.setFont(new  Font("Arial",Font.BOLD,16));
+		usuarioT.setBorder(new LineBorder(Color.BLACK, 1, true));
+		register_conteneiner.add(usuarioT);
+		
+		//nombre de usuario
+		JButton usuarioB= new JButton();
+		usuarioB.setText("Nombre de Usuario");
+		usuarioB.setSize(550, 30);
+		usuarioB.setLocation(20, 115);
+		usuarioB.setBackground(Color.DARK_GRAY);
+		usuarioB.setFont(new Font("Arial",Font.ROMAN_BASELINE,18));
+		usuarioB.setBorder(new LineBorder(Color.DARK_GRAY, 0, false));
+		usuarioB.setEnabled(false);
+		register_conteneiner.add(usuarioB);
+		
+		//biografia
+		JLabel Biografia=new JLabel("Introduzca su Biografia");
+		Biografia.setSize(500,50);
+		Biografia.setLocation(60,190);
+		Biografia.setBackground(Color.white);
+		Biografia.setHorizontalAlignment(JLabel.CENTER);
+		Biografia.setBackground(null);
+		Biografia.setOpaque(true);
+		Biografia.setFont(new Font("Arial",Font.ITALIC,32));
+		register_conteneiner.add(Biografia);
+		
+		//cuadro de texto
+		JTextArea BiografiaT = new JTextArea();
+		BiografiaT.setSize(475,85);
+		BiografiaT.setLocation(60,240);
+		BiografiaT.setFont(new Font("Arial", Font.PLAIN, 13));
+		BiografiaT.setLineWrap(true);   
+		BiografiaT.setBorder(new LineBorder(Color.BLACK, 3, true));
+		BiografiaT.setWrapStyleWord(true);
+		register_conteneiner.add(BiografiaT);
+		
+		/*PREFERENCIAS*/
+		//biografia
+		JLabel Preferencias=new JLabel("Preferencias");
+		Preferencias.setSize(500,50);
+		Preferencias.setLocation(228,320);
+		Preferencias.setBackground(Color.BLACK);
+		Preferencias.setBackground(null);
+		Preferencias.setOpaque(true);
+		Preferencias.setFont(new Font("Arial",Font.ITALIC,22));
+		register_conteneiner.add(Preferencias);
+		
+		//preferencias
+		JCheckBox checkBoxS=new JCheckBox();
+		checkBoxS.setSize(18, 18);
+		checkBoxS.setLocation(20,370);
+		checkBoxS.setBackground(Color.LIGHT_GRAY);
+		register_conteneiner.add(checkBoxS);
+		//text
+		JLabel checkBoxST = new JLabel();
+		checkBoxST.setText("Salado");
+		checkBoxST.setSize(60, 14);
+		checkBoxST.setOpaque(true);
+		checkBoxST.setBackground(null);
+		checkBoxST.setForeground(Color.BLACK);
+		checkBoxST.setLocation(50,370);
+		checkBoxST.setFont(new  Font("Arial",Font.ITALIC,14));
+		register_conteneiner.add(checkBoxST);
+		
+		// CHECKBOX DULCE
+		JCheckBox checkBoxD=new JCheckBox();
+		checkBoxD.setSize(18, 18);
+		checkBoxD.setLocation(250,370);
+		checkBoxD.setBackground(Color.LIGHT_GRAY);
+		register_conteneiner.add(checkBoxD);
+		//text
+		JLabel checkBoxDT = new JLabel();
+		checkBoxDT.setText("Salado");
+		checkBoxDT.setSize(60, 14);
+		checkBoxDT.setOpaque(true);
+		checkBoxDT.setBackground(null);
+		checkBoxDT.setForeground(Color.BLACK);
+		checkBoxDT.setLocation(270,370);
+		checkBoxDT.setFont(new  Font("Arial",Font.ITALIC,14));
+		register_conteneiner.add(checkBoxDT);
+		
+		// CHECKBOX ACIDO
+		JCheckBox checkBoxA=new JCheckBox();
+		checkBoxA.setSize(18, 18);
+		checkBoxA.setLocation(480,370);
+		checkBoxA.setBackground(Color.LIGHT_GRAY);
+		register_conteneiner.add(checkBoxA);
+		//text
+		JLabel checkBoxAT = new JLabel();
+		checkBoxAT.setText("Acido");
+		checkBoxAT.setSize(60, 14);
+		checkBoxAT.setOpaque(true);
+		checkBoxAT.setBackground(null);
+		checkBoxAT.setForeground(Color.BLACK);
+		checkBoxAT.setLocation(500,370);
+		checkBoxAT.setFont(new  Font("Arial",Font.ITALIC,14));
+		register_conteneiner.add(checkBoxAT);
+		
+		//boton aceptar
+		JButton terminos= new JButton();
+		terminos.setText("Acepto los terminos");
+		terminos.setSize(500, 20);
+		terminos.setBackground(Color.WHITE);
+		terminos.setFont(new Font("Arial",Font.ROMAN_BASELINE,14));
+		terminos.setBorder(new LineBorder(Color.BLACK, 2, true));
+		terminos.setLocation(50,400);
+		register_conteneiner.add(terminos);
+				
+		ButtonGroup opciones = new ButtonGroup();
+		JRadioButton acepto =new JRadioButton();
+		acepto.setSize(90,16);
+		acepto.setFont(new Font("Arial",Font.ROMAN_BASELINE,16));
+		acepto.setText("Acepto");
+		acepto.setBackground(Color.LIGHT_GRAY);
+		acepto.setLocation(140,430);
+		JRadioButton deniego =new JRadioButton();
+		deniego.setSize(130,16);
+		deniego.setFont(new Font("Arial",Font.ROMAN_BASELINE,16));
+		deniego.setText("No acepto");
+		deniego.setBackground(Color.LIGHT_GRAY);
+		deniego.setLocation(360,430);
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		opciones.add(acepto);
+		opciones.add(deniego);
+		acepto.setSelected(true);
+        setVisible(true);
+        register_conteneiner.add(acepto);
+        register_conteneiner.add(deniego);
+
 		
 		
 		
